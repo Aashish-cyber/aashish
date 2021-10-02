1e5fd0e93540ea95a1825d58ea316376e88877d9
Aashish Kumar <ranaaashish000@gmail.com>
2021-10-02 16:41:54 +0530

    added implementaion for Diff.java

---
+++ Diff.java
@@ -0,0 +1,8 @@
+public class Diff {
+    public static void main(string[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("The divide is " + (b/a));
+        
+    }
+}
